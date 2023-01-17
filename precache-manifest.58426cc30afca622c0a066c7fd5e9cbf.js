329ad7271333b84664792109af64db23094c415b
Iryna Bolbat <irinabolbat98@gmail.com>
2023-01-17 12:36:35 +0200

    Updates

---
+++ precache-manifest.58426cc30afca622c0a066c7fd5e9cbf.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = [
+  {
+    "revision": "35aac690cfdf2c8b69b5",
+    "url": "/games/static/css/main.126b7af2.chunk.css"
+  },
+  {
+    "revision": "35aac690cfdf2c8b69b5",
+    "url": "/games/static/js/main.35aac690.chunk.js"
+  },
+  {
+    "revision": "0175a56d7ad777018231",
+    "url": "/games/static/css/1.1aba6b7f.chunk.css"
+  },
+  {
+    "revision": "0175a56d7ad777018231",
+    "url": "/games/static/js/1.0175a56d.chunk.js"
+  },
+  {
+    "revision": "9061fa9166f9142481fd",
+    "url": "/games/static/js/runtime~main.9061fa91.js"
+  },
+  {
+    "revision": "305e3a8735d493fd032199d67c47cc44",
+    "url": "/games/static/media/memoryGame.305e3a87.png"
+  },
+  {
+    "revision": "09323ae0d472eb0ccf494bcbb4baa5f7",
+    "url": "/games/static/media/sudoku.09323ae0.png"
+  },
+  {
+    "revision": "ed03f3560ba636bcd8b77917b59c6b39",
+    "url": "/games/static/media/giraffe.ed03f356.jpeg"
+  },
+  {
+    "revision": "f2d77a7cbfb4ed578e68bd51c0b9e078",
+    "url": "/games/static/media/parrots.f2d77a7c.jpeg"
+  },
+  {
+    "revision": "252e10c3122dea3ce444a58adea4a9ff",
+    "url": "/games/static/media/peacock.252e10c3.jpeg"
+  },
+  {
+    "revision": "cea441ee434b91b535f30c8df6c99012",
+    "url": "/games/static/media/raccoon.cea441ee.jpeg"
+  },
+  {
+    "revision": "d003872e27f7b33c1e963bdacaec1859",
+    "url": "/games/static/media/red-panda.d003872e.jpeg"
+  },
+  {
+    "revision": "423da3926ce2de1a073a61f823c363e9",
+    "url": "/games/static/media/tiger.423da392.jpeg"
+  },
+  {
+    "revision": "f954b75ad0a067fa4efee33c40184df0",
+    "url": "/games/static/media/hedgehog.f954b75a.jpeg"
+  },
+  {
+    "revision": "141769061bbce3e2a8b8735bca6a584f",
+    "url": "/games/static/media/squirrel.14176906.jpeg"
+  },
+  {
+    "revision": "f086a571df349c8cd706fcd705d409d0",
+    "url": "/games/static/media/owl.f086a571.jpeg"
+  },
+  {
+    "revision": "290a8741b7b1c30c5e47748d2b50deaf",
+    "url": "/games/static/media/white_tigers.290a8741.jpeg"
+  },
+  {
+    "revision": "b28c825464f8ef4e2b81e8f7ddac493f",
+    "url": "/games/static/media/elephant.b28c8254.jpeg"
+  },
+  {
+    "revision": "b5a144b9ddc2468be6ba86413298d8f8",
+    "url": "/games/static/media/rhinoceros.b5a144b9.png"
+  },
+  {
+    "revision": "cfa9fdd330c591303c6163157b164feb",
+    "url": "/games/static/media/last_free_cell.cfa9fdd3.png"
+  },
+  {
+    "revision": "4532517eee82d49ec80eecb488cf9c28",
+    "url": "/games/static/media/last_free_cell_1.4532517e.png"
+  },
+  {
+    "revision": "b969042a28e42c12330b60a8eabc913d",
+    "url": "/games/static/media/last_free_cell_2.b969042a.png"
+  },
+  {
+    "revision": "aee6a42908572b0a093f12784b1f42ca",
+    "url": "/games/static/media/last_free_cell_3.aee6a429.png"
+  },
+  {
+    "revision": "580791d504f0c3b141200f7dedafd2e1",
+    "url": "/games/static/media/last_free_cell_4.580791d5.png"
+  },
+  {
+    "revision": "5525e380a0b6097cd55bcf74dcf844df",
+    "url": "/games/static/media/last_remaining_cell.5525e380.png"
+  },
+  {
+    "revision": "32703503c6fe5f963497e04e52101b30",
+    "url": "/games/static/media/last_remaining_cell_1.32703503.png"
+  },
+  {
+    "revision": "e01267268f2dda7dcbbb22172b10cf24",
+    "url": "/games/static/media/last_remaining_cell_2.e0126726.png"
+  },
+  {
+    "revision": "c12bcd68db9e2639dcea73168b32f972",
+    "url": "/games/static/media/last_possible_number_1.c12bcd68.png"
+  },
+  {
+    "revision": "804a98a68cd0157c71d06c60e9ce2050",
+    "url": "/games/static/media/last_possible_number_2.804a98a6.png"
+  },
+  {
+    "revision": "0be10ca63f9bf13159c79cbdb2350586",
+    "url": "/games/static/media/obvious_singles_technique.0be10ca6.png"
+  },
+  {
+    "revision": "8d7fcd55d8271fc5a2b7ffa5f3dc4faa",
+    "url": "/games/static/media/obvious_singles_1.8d7fcd55.png"
+  },
+  {
+    "revision": "92712d109944f5bd37d753f0ab423f68",
+    "url": "/games/static/media/obvious_singles_2.92712d10.png"
+  },
+  {
+    "revision": "429d95397bd36a69dd0d3ac73327fc75",
+    "url": "/games/static/media/obvious_pairs_1.429d9539.png"
+  },
+  {
+    "revision": "54a0301a20bc8e5cbab28155ba05c235",
+    "url": "/games/static/media/obvious_triples_1.54a0301a.png"
+  },
+  {
+    "revision": "ca2459e93f00395ee62a8e5db37a43c9",
+    "url": "/games/static/media/obvious_triples_2.ca2459e9.png"
+  },
+  {
+    "revision": "9dbd7dad582c31e675e2711a6e9e168d",
+    "url": "/games/static/media/obvious_triples_3.9dbd7dad.png"
+  },
+  {
+    "revision": "eb5fb917ba1a5107412bdf155272ae17",
+    "url": "/games/static/media/hidden_singles.eb5fb917.png"
+  },
+  {
+    "revision": "8776137aade14f2ce16c4bd12846dd19",
+    "url": "/games/static/media/hidden_singles_1.8776137a.png"
+  },
+  {
+    "revision": "6d79fae9597429ccf93736b20a5a9283",
+    "url": "/games/static/media/hidden_singles_2.6d79fae9.png"
+  },
+  {
+    "revision": "df0de3abd182936cd3e9d60731099c35",
+    "url": "/games/static/media/hidden_pairs_1.df0de3ab.png"
+  },
+  {
+    "revision": "16ead06bf44b4d81b622ef514f8edcbc",
+    "url": "/games/static/media/hidden_pairs_2.16ead06b.png"
+  },
+  {
+    "revision": "ed7d0b6e9a58551e02e0e4976d84b48f",
+    "url": "/games/static/media/hidden_triples_1.ed7d0b6e.png"
+  },
+  {
+    "revision": "a37225b6410c34e2451d15e2b16220de",
+    "url": "/games/static/media/hidden_triples_2.a37225b6.png"
+  },
+  {
+    "revision": "f0d6566523d35a1c50674d7e37858980",
+    "url": "/games/static/media/pointing_pairs_1.f0d65665.png"
+  },
+  {
+    "revision": "95d81c2ff65f71dc0bbe25cd78391cda",
+    "url": "/games/static/media/pointing_pairs_2.95d81c2f.png"
+  },
+  {
+    "revision": "c0048ea6042c3c4bb940eeecd726d994",
+    "url": "/games/static/media/pointing_triples.c0048ea6.png"
+  },
+  {
+    "revision": "618d1c56e2da9fbb24b1ead33e6d8f6e",
+    "url": "/games/static/media/pointing_triples_1.618d1c56.png"
+  },
+  {
+    "revision": "c0ebd90d7c1435381c1e01db2a6cf3c6",
+    "url": "/games/static/media/pointing_triples_2.c0ebd90d.png"
+  },
+  {
+    "revision": "9042d22abca4c964203ebd4d39ecc445",
+    "url": "/games/index.html"
+  }
+];
